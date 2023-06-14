8d45514e9f8362aca34c6541d443e696a7fef0ec
DrgDRGWind <dragonwind22052008@gmail.com>
2023-06-14 18:50:33 +0700

    Edit lan 2

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ def solves(x, y, z):
     global results
     delta = y ** 2 - 4*x*z
     if delta >= 0:
-        if delta == 0:
-            x1 = x2 = -y/2*x
-            results = results + [x1]
+        if x + y + z == 0:
+            x1 = 1
+            x2 = z/x
+        elif x - y + z == 0:
+            x1 = -1
+            x2 = -z/x
         elif delta > 0:
-            x1 = (-y - math.sqrt(delta))/2*x
-            x2 = (-y + math.sqrt(delta))/2*x
-            results = results + [x1] + [x2]
-    elif delta < 0:
-        print("Phuong trinh vo nghiem")
-        results = []
+            x1 = (- y - math.sqrt(delta) ) / 2*x
+            x2 = (- y + math.sqrt(delta) ) / 2*x
+    results = results + [x1] + [x2]
     print(results)
 
 print("Phuong trinh bac hai co dang: ")
